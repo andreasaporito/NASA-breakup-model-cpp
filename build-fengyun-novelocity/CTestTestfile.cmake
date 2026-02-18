176ddbe5cd20c8b305ab39376489a6d697931727
andreasaporito <andrea.saporito03@gmail.com>
2026-02-18 14:10:06 +0100

    dummy tested to stress results

---
+++ build-fengyun-novelocity/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/andrea/LSMS_project/NASA-breakup-model-cpp
+# Build directory: /home/andrea/LSMS_project/NASA-breakup-model-cpp/build-fengyun-novelocity
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("_deps/yaml-cpp-build")
+subdirs("_deps/spdlog-build")
+subdirs("test")
